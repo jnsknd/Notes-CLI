231d564d5d45d01daf8ae5fcdb886ef38bbd0d3b
joanskenderi <jnsknd.dev@gmail.com>
2025-07-31 14:16:37 +0200

    feat: add web interface

--- src/web/server.js
+++ src/web/server.js
@@ -29,9 +29,20 @@ const createServer = (notes) => {
     return http.createServer(async (req, res) => {
         const __filename = fileURLToPath(import.meta.url);
         const __dirname = path.dirname(__filename);
+
         const HTML_PATH = path.join(__dirname, 'templates', 'template.html');
         const template = await fs.readFile(HTML_PATH, 'utf-8');
         const html = interpolate(template, { notes: formatNotes(notes) });
+
+        if (req.url === '/css/styles.css') {
+            const CSS_PATH = path.join(__dirname, 'css', 'styles.css');
+            const css = await fs.readFile(CSS_PATH, 'utf-8');
+
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            res.end(css);
+
+            return;
+        }
 
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(html);
@@ -43,7 +54,7 @@ export const start = (notes, port) => {
 
     server.listen(port, () => {
         const address = `http://localhost:${port}`;
-        console.log(`Server is listening on ${address}`);
+        console.log(`Server running at ${address}`);
         open(address);
     });
 };
